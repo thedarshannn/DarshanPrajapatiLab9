42a0f068cf6641448c054086d79dd1a0174939e1
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-17 16:10:55 -0400

    Changed module name and home screen icon.

---
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity9.java
@@ -0,0 +1,15 @@
+// Darshan Prajapati - n01584247
+package darshan.prajapati.n01584247.dp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrajapatiActivity9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
