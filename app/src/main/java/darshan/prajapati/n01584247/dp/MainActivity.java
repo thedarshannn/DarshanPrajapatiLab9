00a0b943cc2e1eb754f6233762c19c7da4783380
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-17 16:04:42 -0400

    Initial Commit.

---
+++ app/src/main/java/darshan/prajapati/n01584247/dp/MainActivity.java
@@ -0,0 +1,14 @@
+package darshan.prajapati.n01584247.dp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/darshan/prajapati/n01584247/dp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package darshan.prajapati.n01584247.dp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
