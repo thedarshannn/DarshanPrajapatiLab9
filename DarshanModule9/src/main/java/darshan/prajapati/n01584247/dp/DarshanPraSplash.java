9c9f08053e177c68e0580f7217d51f6d8eeb2576
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-17 21:10:50 -0400

    Implemented splash screen.

---
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/DarshanPraSplash.java
@@ -0,0 +1,26 @@
+package darshan.prajapati.n01584247.dp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DarshanPraSplash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(DarshanPraSplash.this, PrajapatiActivity9.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
