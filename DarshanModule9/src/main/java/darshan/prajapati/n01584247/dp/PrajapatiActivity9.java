b0387ec77bff7a4a857eac2c739dbe07034c4c0c
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-17 21:53:38 -0400

    Added logic to set Darshan1Fragment as default.

--- DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity9.java
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity9.java
@@ -1,15 +1,63 @@
 // Darshan Prajapati - n01584247
 package darshan.prajapati.n01584247.dp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class PrajapatiActivity9 extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class PrajapatiActivity9 extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = findViewById(R.id.darToolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.darNavView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.darFragmentContainer, new Darshan1Fragment()).commit();
+            navigationView.setCheckedItem(R.id.darNavHome);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.darNavHome) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.darFragmentContainer, new Darshan1Fragment())
+                    .commit();
+        } else if (itemId == R.id.darNavSettings) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.darFragmentContainer, new Prajapati2Fragment())
+                    .commit();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
