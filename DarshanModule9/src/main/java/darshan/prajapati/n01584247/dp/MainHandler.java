36f529075ac990c88a51beb2cb8e234dbd0da489
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-18 18:57:42 -0400

    Added MainHandler class and functionality to search button.

--- DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/MainHandler.java
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/MainHandler.java
@@ -1,11 +1,14 @@
 package darshan.prajapati.n01584247.dp;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -45,8 +48,47 @@ public class MainHandler extends AppCompatActivity {
             editor.putInt(getString(R.string.mode), AppCompatDelegate.getDefaultNightMode());
             editor.apply();
             return true;
+        } else if (id == R.id.darSearch) {
+            showSearchDialog();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSearchDialog() {
+        // Create an AlertDialog to prompt the user to enter search phrase
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.search);
+        final EditText input = new EditText(this);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton(R.string.search, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String searchPhrase = input.getText().toString().trim();
+                if (!searchPhrase.isEmpty()) {
+                    launchGoogleSearch(searchPhrase);
+                } else {
+                    Toast.makeText(MainHandler.this, getString(R.string.search_msg), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Show the AlertDialog
+        builder.show();
+    }
+
+    private void launchGoogleSearch(String searchPhrase) {
+
+        // Create an Intent to perform a web search
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        intent.putExtra(getString(R.string.query), searchPhrase);
+        startActivity(intent);
+
+        // Hide the keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
 }
