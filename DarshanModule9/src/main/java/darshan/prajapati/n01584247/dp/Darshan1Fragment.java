88983bf500dca7e545c86707e6c5d92719859011
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-17 21:40:40 -0400

    Designed navigation drawer.

---
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/Darshan1Fragment.java
@@ -0,0 +1,30 @@
+package darshan.prajapati.n01584247.dp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Darshan1Fragment extends Fragment {
+
+    public Darshan1Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_darshan1, container, false);
+    }
+}
---
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/Prajapati2Fragment.java
@@ -0,0 +1,28 @@
+package darshan.prajapati.n01584247.dp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Prajapati2Fragment extends Fragment {
+
+    public Prajapati2Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prajapati2, container, false);
+    }
+}
