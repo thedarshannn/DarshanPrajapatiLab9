286432d986f165ecbbe8c0f0effc78093f4be539
Darshankumar Alpeshbhai Prajapati <mr.darshan2919@gmail.com>
2024-03-18 18:32:58 -0400

    Modify the menu toolbar.

--- DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity9.java
+++ DarshanModule9/src/main/java/darshan/prajapati/n01584247/dp/PrajapatiActivity9.java
@@ -8,7 +8,10 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,7 +23,6 @@ public class PrajapatiActivity9 extends AppCompatActivity implements NavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         Toolbar toolbar = findViewById(R.id.darToolbar);
         setSupportActionBar(toolbar);
@@ -64,7 +66,17 @@ public class PrajapatiActivity9 extends AppCompatActivity implements NavigationV
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem darMenuItem = menu.findItem(R.id.darToggle);
+        MenuItem praMenuItem = menu.findItem(R.id.darSearch);
+
+        SpannableString darText = new SpannableString(darMenuItem.getTitle());
+        SpannableString praText = new SpannableString(praMenuItem.getTitle());
+        darText.setSpan(new ForegroundColorSpan(Color.RED), 0, darText.length(), 0);
+        praText.setSpan(new ForegroundColorSpan(Color.RED), 0, praText.length(), 0);
+        darMenuItem.setTitle(darText);
+        praMenuItem.setTitle(praText);
+
         return true;
     }
-
 }
